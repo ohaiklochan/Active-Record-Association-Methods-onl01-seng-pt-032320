818e80d6abd53c6574ce6c244c9a41fdea7fd203
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 03:19:23 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -7,7 +7,6 @@ class Song < ActiveRecord::Base
   end
 
   def drake_made_this
-    self.artist = Artist.find_or_create_by(:name)
-
+    self.artist = Artist.find_or_create_by(name: "Drake")
   end
 end
