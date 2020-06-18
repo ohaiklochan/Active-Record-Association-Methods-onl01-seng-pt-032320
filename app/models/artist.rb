45e582f5cfd794294ac0de97a1dadda0617a1e5a
Chloe Steffens <caforonda103@yahoo.com>
2020-06-18 03:08:18 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,11 +3,11 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.songs.first
+    self.songs.first.genre
   end
 
   def song_count
-    #return the number of songs associated with the artist
+    self.
   end
 
   def genre_count
